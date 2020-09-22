ea75e3ef2e8df1dc80a3b66ae5172d9215bf279d
Teurowel <teurowel@gmail.com>
2020-09-22 17:46:37 +0900

    New client get all existed client info

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -8,42 +8,58 @@ using System.Net;
 
 public class NetworkMan : MonoBehaviour
 {
+    //Like socket in python
     public UdpClient udp;
     // Start is called before the first frame update
     void Start()
     {
+        //Create socket
         udp = new UdpClient();
-        
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
 
+        //Set socket to connet server
+        //udp.Connect("ec2-3-15-221-96.us-east-2.compute.amazonaws.com", 12345);
+        udp.Connect("localhost", 12345);
+
+
+        //Send data
+        //We can only send Byte type so we need to convert data to Bytes
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
-      
         udp.Send(sendBytes, sendBytes.Length);
 
+
+        //Make OnReceived Function to handle all receving data, pass argument for OnReceived function
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
 
         InvokeRepeating("HeartBeat", 1, 1);
     }
 
-    void OnDestroy(){
+    void OnDestroy()
+    {
         udp.Dispose();
     }
 
 
-    public enum commands{
+
+
+    public enum commands
+    {
         NEW_CLIENT,
-        UPDATE
+        UPDATE,
+        All_CLIENT_INFO
     };
     
     [Serializable]
-    public class Message{
+    public class Message
+    {
         public commands cmd;
     }
     
     [Serializable]
-    public class Player{
+    public class Player
+    {
         [Serializable]
-        public struct receivedColor{
+        public struct receivedColor
+        {
             public float R;
             public float G;
             public float B;
@@ -53,70 +69,123 @@ public class NetworkMan : MonoBehaviour
     }
 
     [Serializable]
-    public class NewPlayer{
+    public class NewPlayer
+    {
         
     }
 
     [Serializable]
-    public class GameState{
+    public class GameState
+    {
         public Player[] players;
+    }
+
+    [Serializable]
+    public class ClientInfo
+    {
+        public string IP;
+        public int PORT;
+    }
+
+    [Serializable]
+    public class AllClientsInfo
+    {
+        public int numOfClients;
+
+        public ClientInfo[] allClients;
     }
 
     public Message latestMessage;
     public GameState lastestGameState;
-    void OnReceived(IAsyncResult result){
-        // this is what had been passed into BeginReceive as the second parameter:
+    public AllClientsInfo lastestAllClietnsInfo;
+    void OnReceived(IAsyncResult result)
+    {
+        // this is what had been passed into BeginReceive as the second parameter from BeginReceive function:
         UdpClient socket = result.AsyncState as UdpClient;
         
         // points towards whoever had sent the message:
+        //This will be information of who sent the message
         IPEndPoint source = new IPEndPoint(0, 0);
 
         // get the actual message and fill out the source:
+        //using socket, get last data,
+        //when EndReceive is called, we stop handle next message until handling current message and continue when BeginReceive called
         byte[] message = socket.EndReceive(result, ref source);
-        
+
+
         // do what you'd like with `message` here:
+        //convert byte[] data(jason) to string
         string returnData = Encoding.ASCII.GetString(message);
-        Debug.Log("Got this: " + returnData);
+        //Debug.Log("Got this: " + returnData);
         
+        //convert string to Message class
         latestMessage = JsonUtility.FromJson<Message>(returnData);
-        try{
-            switch(latestMessage.cmd){
+        try
+        {
+            //What kind of message is this?
+            switch(latestMessage.cmd)
+            {
+                //New client connected
                 case commands.NEW_CLIENT:
                     break;
+                //Update game with new info
                 case commands.UPDATE:
                     lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    break;
+                //Get all clients info
+                case commands.All_CLIENT_INFO:
+                    lastestAllClietnsInfo = JsonUtility.FromJson<AllClientsInfo>(returnData);
+                    string msg = "All clients list\n";
+
+                    msg += "Number of clients: " + lastestAllClietnsInfo.numOfClients.ToString() + "\n";
+
+                    for (int i = 0; i < lastestAllClietnsInfo.numOfClients; ++i)
+                    {
+                        msg += "Client" + i + "\n";
+                        msg += "IP: " + lastestAllClietnsInfo.allClients[i].IP + "\n";
+                        msg += "PORT: " + lastestAllClietnsInfo.allClients[i].PORT + "\n\n";
+                    }
+
+                    Debug.Log(msg);
                     break;
                 default:
                     Debug.Log("Error");
                     break;
             }
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             Debug.Log(e.ToString());
         }
         
         // schedule the next receive operation once reading is done:
+        //continue get next message
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
+    void SpawnPlayers()
+    {
 
     }
 
-    void UpdatePlayers(){
+    void UpdatePlayers()
+    {
 
     }
 
-    void DestroyPlayers(){
+    void DestroyPlayers()
+    {
 
     }
     
-    void HeartBeat(){
+    void HeartBeat()
+    {
         Byte[] sendBytes = Encoding.ASCII.GetBytes("heartbeat");
         udp.Send(sendBytes, sendBytes.Length);
     }
 
-    void Update(){
+    void Update()
+    {
         SpawnPlayers();
         UpdatePlayers();
         DestroyPlayers();
